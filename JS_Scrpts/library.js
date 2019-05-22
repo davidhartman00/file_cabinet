3b588cdf428a52e7dcf0ac92d10fc818b02ff9a3
davidhartman00 <davidhartman00@gmail.com>
2019-05-22 14:36:48 -0700

    added arrayOfPrimes

--- JS_Scrpts/library.js
+++ JS_Scrpts/library.js
@@ -4,3 +4,26 @@ Date Created.: 05/22/2019
 Author.......: David Hartman
 Description..: A collection of useful Javascript fuctions and methods
 */
+
+// returns an array of all prime numbers less then the number provided.
+// As 1 is not prime per the exact definition for prime, the array does
+// not include 1.
+const arrayOfPrimes = num => {
+    let arr = [], i = 2
+
+    while (i <= num) {
+        let a = 2, flag = true;
+        if (i > 2) {
+            while (a < i) {
+                if (i % a === 0) {
+                    flag = false
+                    break
+                }
+                a++
+            }
+        }
+        if (flag) arr.push(i)
+        i++
+    }
+    return arr
+}
