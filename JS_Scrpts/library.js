e9bfc59a2d41100b4f549ac0f0cbd73cfc954f03
davidhartman00 <davidhartman00@gmail.com>
2019-05-22 14:41:10 -0700

    added arrFibonacci function

--- JS_Scrpts/library.js
+++ JS_Scrpts/library.js
@@ -8,7 +8,7 @@ Description..: A collection of useful Javascript fuctions and methods
 // returns an array of all prime numbers less then the number provided.
 // As 1 is not prime per the exact definition for prime, the array does
 // not include 1.
-const arrayOfPrimes = num => {
+const arrPrimes = num => {
     let arr = [], i = 2
 
     while (i <= num) {
@@ -23,6 +23,21 @@ const arrayOfPrimes = num => {
             }
         }
         if (flag) arr.push(i)
+        i++
+    }
+    return arr
+}
+
+
+// returns an array of all numbers in the Fibonacci 
+// sequence less then the number provided.
+const arrFibonacci = num => {
+    let a = 1, b = 0, arr = [1], i = 0
+    while (i < num) {
+        let x = a + b
+        arr.push(x)
+        b = a
+        a = x
         i++
     }
     return arr
